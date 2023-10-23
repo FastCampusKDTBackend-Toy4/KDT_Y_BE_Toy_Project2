a76949cab4e95d007c79b2feff28760d17c4fe6e
rightpair <tkddn204@gmail.com>
2023-10-23 18:41:58 +0900

    [feat] 여행 엔티티 개발
    Motivation:
    - 주어진 요구사항에 따라 여행 엔티티를 개발
    - 여행 엔티티에 포함된 국내외여부를 `TripType` 클래스로 개발
    
    Modification:
    - 여행 엔티티 클래스(`Trip.java`) 추가
    - 국내외여부 enum(`TripType.java`) 추가
    - 해당 디렉터리 사용으로 인한 `.gitkeep` 파일 삭제

---
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/domain/Trip.java
@@ -0,0 +1,42 @@
+package com.kdt_y_be_toy_project2.domain.trip.domain;
+
+import com.kdt_y_be_toy_project2.domain.model.TimeScheduleInfo;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "trip")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Trip {
+
+    @Id
+    @Column(name = "trip_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "trip_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TripType tripType;
+
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "start_date_time", column = @Column(name = "startDateTime", nullable = false)),
+        @AttributeOverride(name = "end_date_time", column = @Column(name = "endDateTime", nullable = false))
+    })
+    private TimeScheduleInfo tripSchedule;
+
+    @Builder
+    private Trip(Long id, String name, TripType tripType, TimeScheduleInfo tripSchedule) {
+        this.id = id;
+        this.name = name;
+        this.tripType = tripType;
+        this.tripSchedule = tripSchedule;
+    }
+}
