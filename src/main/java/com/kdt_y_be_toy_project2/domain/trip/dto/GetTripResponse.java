5dbc5dd73202ca73c443edd1a5379fc959a18cb5
rightpair <tkddn204@gmail.com>
2023-10-24 18:28:31 +0900

    [refactor] 여행 조회 응답 DTO를 class에서 record로 수정
    Motivation:
    - 팀원들과의 회의에서 DTO를 record로 개발하자는 의견이 나왔습니다.
    - 따라서 개발했던 여행 조회 응답 DTO를 class에서 record로 변경하려 합니다.
    
    Modification:
    - `GetTripResponse.java`를 class에서 record로 변경
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/kdt_y_be_toy_project2/domain/trip/dto/GetTripResponse.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/dto/GetTripResponse.java
@@ -4,24 +4,14 @@ import com.kdt_y_be_toy_project2.domain.model.TimeScheduleInfo;
 import com.kdt_y_be_toy_project2.domain.trip.domain.Trip;
 import com.kdt_y_be_toy_project2.domain.trip.domain.TripType;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-public class GetTripResponse {
-
-    private final Long tripId;
-    private final String name;
-    private final TimeScheduleInfo tripScheduleInfo;
-    private final TripType tripType;
-
-    @Builder
-    private GetTripResponse(Long tripId, String name, TimeScheduleInfo tripScheduleInfo, TripType tripType) {
-        this.tripId = tripId;
-        this.name = name;
-        this.tripScheduleInfo = tripScheduleInfo;
-        this.tripType = tripType;
-    }
-
+@Builder
+public record GetTripResponse(
+        Long tripId,
+        String name,
+        TimeScheduleInfo tripScheduleInfo,
+        TripType tripType
+) {
     public static GetTripResponse from(Trip trip) {
         return GetTripResponse.builder()
                 .tripId(trip.getId())
