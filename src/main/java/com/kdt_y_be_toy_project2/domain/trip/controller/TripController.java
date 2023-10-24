26cb26adc57ef4f589e049d648b672b4ea869a3d
rightpair <tkddn204@gmail.com>
2023-10-24 16:49:49 +0900

    [feat] `TripController.java`에 여행 등록과 수정 관련 메소드 추가
    Motivation:
    - 컨트롤러에서 요구사항의 여행 등록과 수정에 관련된 메소드들을 개발하려 합니다.
    
    Modification:
    - `TripController.java`의 createTrip 메소드 개발
    - `TripController.java`의 editTrip 메소드 개발
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/kdt_y_be_toy_project2/domain/trip/controller/TripController.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/controller/TripController.java
@@ -1,15 +1,12 @@
 package com.kdt_y_be_toy_project2.domain.trip.controller;
 
-import com.kdt_y_be_toy_project2.domain.trip.dto.GetTripResponse;
+import com.kdt_y_be_toy_project2.domain.trip.dto.*;
 import com.kdt_y_be_toy_project2.domain.trip.service.TripService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,10 +25,28 @@ public class TripController {
 
     @GetMapping("/trips/{trip_id}")
     public ResponseEntity<GetTripResponse> getTripById(
-            @PathVariable(name = "trip_id") final Long id
+            @PathVariable(name = "trip_id") final Long tripId
     ) {
         return ResponseEntity.status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(tripService.getTripById(id));
+                .body(tripService.getTripById(tripId));
+    }
+
+    @PostMapping("/trips")
+    public ResponseEntity<CreateTripResponse> createTrip(
+            @RequestBody final CreateTripRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(tripService.createTrip(request));
+    }
+
+    @PutMapping("/trips/{trip_id}")
+    public ResponseEntity<EditTripResponse> editTrip(
+            @RequestBody final EditTripRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(tripService.editTrip(request));
     }
 }
