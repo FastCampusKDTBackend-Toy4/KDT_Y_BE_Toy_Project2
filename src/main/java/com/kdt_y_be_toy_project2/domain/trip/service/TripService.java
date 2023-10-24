1d449799cd847ebef44865690a74d78ac2bbaf0f
rightpair <tkddn204@gmail.com>
2023-10-24 18:31:11 +0900

    [feat] `TripService` 임시 클래스 파일 생성
    Motivation:
    - 컨트롤러 개발에 있어서 컴파일 에러 방지를 위해 임시 객체를 개발하려 합니다.
    - 해당 객체는 #16을 해결할 때 개발이 완료될 것입니다.
    
    Modification:
    - `TripService` 임시 클래스 파일 생성
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

---
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/service/TripService.java
@@ -0,0 +1,38 @@
+package com.kdt_y_be_toy_project2.domain.trip.service;
+
+import com.kdt_y_be_toy_project2.domain.trip.dto.*;
+import com.kdt_y_be_toy_project2.domain.trip.repository.TripRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TripService {
+
+    private final TripRepository tripRepository;
+
+    @Transactional(readOnly = true)
+    public List<GetTripResponse> getAllTrips() {
+        return null;
+    }
+
+    @Transactional(readOnly = true)
+    public GetTripResponse getTripById(final Long id) {
+        return null;
+    }
+
+    public CreateTripResponse createTrip(CreateTripRequest request) {
+        return null;
+    }
+
+    public EditTripResponse editTrip(EditTripRequest request) {
+        return null;
+    }
+}
+
+
+
