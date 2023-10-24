f3c92ee2149c6913b53c17ff3f8396be785ba4fb
Nine-JH <rnwkgus2@gmail.com>
2023-10-24 14:35:25 +0900

    [feat] GlobalExceptionHandler 추가
    Motivation:
    - Controller 요청 시 여러 예외 상황들을 적절하게 Handling 해줄 필요성이 생겼습니다.
    
    - Spring 에서는 `@ControllerAdvice`를 통해 이를 해결하고 있습니다.
    Modification:
    - `GlobalExceptionHandler.java` 생성.

---
+++ src/main/java/com/kdt_y_be_toy_project2/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.kdt_y_be_toy_project2.global.error;
+
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleApplicationException(ApplicationException ex) {
+        log.error("ApplicationError : {}", ex.getMessage());
+        return ResponseEntity.status(ex.getErrorCode().getHttpStatus())
+            .body(new ErrorResponse(LocalDateTime.now(), ex.getErrorCode().getSimpleMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleDataAccessException(DataAccessException ex) {
+        log.error(ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ErrorResponse(LocalDateTime.now(), "알 수 없는 오류입니다. 다음에 시도해주세요."));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleInternalServerError(RuntimeException ex) {
+        log.error(ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ErrorResponse(LocalDateTime.now(), "알 수 없는 오류입니다. 다음에 시도해주세요."));
+    }
+}
