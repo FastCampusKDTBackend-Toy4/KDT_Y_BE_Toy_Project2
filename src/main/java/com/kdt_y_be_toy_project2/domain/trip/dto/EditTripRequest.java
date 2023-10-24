f70c0cbc5962b40a42e2f4f8bdc3f2346693429f
rightpair <tkddn204@gmail.com>
2023-10-24 18:26:12 +0900

    [feat] 여행 수정 관련 요청과 응답 DTO 개발
    Motivation:
    - 요구사항에 기재된 여행 수정에 있어서 필요한 항목들을 포함하는 요청 및 응답 DTO를 개발하려 합니다.
    
    Modification:
    - `EditTripRequest.java` 개발
    - `EditTripResponse.java` 개발
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

---
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/dto/EditTripRequest.java
@@ -0,0 +1,27 @@
+package com.kdt_y_be_toy_project2.domain.trip.dto;
+
+import com.kdt_y_be_toy_project2.domain.trip.domain.TripType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record EditTripRequest(
+        @NotBlank
+        String name,
+
+        @NotNull
+        @DateTimeFormat(pattern = "YYYY-MM-DD HH:mm")
+        LocalDateTime startDateTime,
+
+        @NotNull
+        @DateTimeFormat(pattern = "YYYY-MM-DD HH:mm")
+        LocalDateTime endDateTime,
+
+        @NotNull
+        TripType tripType
+) {
+}
---
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/dto/EditTripResponse.java
@@ -0,0 +1,14 @@
+package com.kdt_y_be_toy_project2.domain.trip.dto;
+
+import com.kdt_y_be_toy_project2.domain.trip.domain.TripType;
+
+import java.time.LocalDateTime;
+
+public record EditTripResponse(
+        Long id,
+        String name,
+        LocalDateTime startDateTime,
+        LocalDateTime endDateTime,
+        TripType tripType
+) {
+}
