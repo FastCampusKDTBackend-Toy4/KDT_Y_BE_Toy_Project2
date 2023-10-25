fca309c4d61b4f0286f65f725db591c9988e29ff
rightpair <tkddn204@gmail.com>
2023-10-25 16:16:32 +0900

    [fix] `@DateTimeFormat` 관련 에러 수정
    Motivation:
    - #30의 내용에 따라 `@DateTimeFormat`을 `@JsonFormat`으로 변경하려 합니다.
    -  또, 변경 전의 날짜 형식이 `YYYY-MM-DD HH:mm`였는데, 정확한 날짜를 입력받기 위해 `yyyy-MM-dd HH:mm`으로 변경하려 합니다.
    
    Modification:
    - 여행 관련 요청 DTO(CreateTripRequest, EditTripRequest)의 @DateTimeFormat을 @JsonFormat으로 변경한다.
    - 잘못된 날짜 포맷을 수정한다.
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/kdt_y_be_toy_project2/domain/trip/dto/CreateTripRequest.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/dto/CreateTripRequest.java
@@ -1,12 +1,12 @@
 package com.kdt_y_be_toy_project2.domain.trip.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kdt_y_be_toy_project2.domain.model.TimeScheduleInfo;
 import com.kdt_y_be_toy_project2.domain.trip.domain.Trip;
 import com.kdt_y_be_toy_project2.domain.trip.domain.TripType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -16,11 +16,11 @@ public record CreateTripRequest(
         String name,
 
         @NotNull(message = "여행 시작 시간을 입력해야 합니다. (예: 2023-10-25 12:00)")
-        @DateTimeFormat(pattern = "YYYY-MM-DD HH:mm")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
         LocalDateTime startDateTime,
 
         @NotNull(message = "여행 종료 시간을 입력해야 합니다. (예: 2023-10-25 12:00)")
-        @DateTimeFormat(pattern = "YYYY-MM-DD HH:mm")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
         LocalDateTime endDateTime,
 
         @NotNull(message = "국내외여부를 입력해야 합니다. (예: DOMESTIC)")
