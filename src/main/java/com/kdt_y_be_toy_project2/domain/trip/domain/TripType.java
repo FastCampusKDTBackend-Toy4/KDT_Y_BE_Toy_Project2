da18d94e528adca1766185a1d6bbba5a59f817d4
rightpair <tkddn204@gmail.com>
2023-10-26 16:04:31 +0900

    [feat] `TripType.java`에 한글 value 추가
    Motivation
    - 사용자가 여행 관련 요청을 보낼 때 TripType을 영어 대문자 형식을 항상 지키면서 요청을 보내야 했기 때문에, 소문자로 입력을 받을 수 없고 가독성도 좋지 않았습니다.
    - 따라서 사용자 친화적인 관점에서 TripType에 한글 value를 추가하려 합니다.
    
    Modification
    - `TripType.java`의 필드에 한글 value 추가
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/kdt_y_be_toy_project2/domain/trip/domain/TripType.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/domain/TripType.java
@@ -1,5 +1,14 @@
 package com.kdt_y_be_toy_project2.domain.trip.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum TripType {
-    DOMESTIC, FOREIGN
+    DOMESTIC("국내"), FOREIGN("국외");
+
+    private final String value;
+
+    TripType(String value) {
+        this.value = value;
+    }
 }
