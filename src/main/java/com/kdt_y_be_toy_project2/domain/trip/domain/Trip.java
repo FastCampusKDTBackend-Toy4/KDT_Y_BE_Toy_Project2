86f359fc1556f4d7e52c3c896bf291dbc842954b
rightpair <tkddn204@gmail.com>
2023-10-26 16:21:34 +0900

    [refactor] `Trip` Entity 수정
    Motivation
    - `Trip` Entity에 불필요한 코드가 있음을 발견하고, 필요한 부분만 남겨놓도록 리팩토링하려 합니다.
    
    Modification
    - [ ] 불필요한 table name 주석 삭제
    - [ ] 불필요한 column name 주석 삭제
    - [ ] `@AttributeOverride`를 올바르게 수정
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/kdt_y_be_toy_project2/domain/trip/domain/Trip.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/domain/Trip.java
@@ -12,17 +12,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "trip")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Trip {
 
     @Id
-    @Column(name = "trip_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(nullable = false)
     private String name;
 
     @Column(name = "trip_type", nullable = false)
@@ -34,11 +32,10 @@ public class Trip {
 
     @Embedded
     @AttributeOverrides({
-            @AttributeOverride(name = "start_date_time", column = @Column(name = "startDateTime", nullable = false)),
-            @AttributeOverride(name = "end_date_time", column = @Column(name = "endDateTime", nullable = false))
+            @AttributeOverride(name = "startDateTime", column = @Column(name = "start_date_time", nullable = false)),
+            @AttributeOverride(name = "endDateTime", column = @Column(name = "end_date_time", nullable = false))
     })
     private TimeScheduleInfo tripSchedule;
-
 
     @Builder
     private Trip(Long id, String name, TripType tripType, TimeScheduleInfo tripSchedule) {
