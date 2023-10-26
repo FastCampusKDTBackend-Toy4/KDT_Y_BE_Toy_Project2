22a7c2259cc011e013ec90fb8c0954f7c9300e40
rightpair <tkddn204@gmail.com>
2023-10-26 12:50:54 +0900

    [refactor] `TripRequest`에 공통 유틸리티 클래스 적용
    Motivation
    - #38 의 개발에 따라 여행 요청 DTO에 공통 유틸리티 클래스를 활용하려 합니다.
    - `@JsonFormat` 어노테이션을 삭제하고, 공통 유틸리티 클래스의 변환 메소드를 사용하려 합니다.
    
    Modification
    - `TripRequest.java` 리팩터링
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/kdt_y_be_toy_project2/domain/trip/dto/TripRequest.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/dto/TripRequest.java
@@ -1,14 +1,12 @@
 package com.kdt_y_be_toy_project2.domain.trip.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kdt_y_be_toy_project2.domain.model.TimeScheduleInfo;
 import com.kdt_y_be_toy_project2.domain.trip.domain.Trip;
 import com.kdt_y_be_toy_project2.domain.trip.domain.TripType;
+import com.kdt_y_be_toy_project2.global.util.DateTimeUtil;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
-
-import java.time.LocalDateTime;
 
 @Builder
 public record TripRequest(
@@ -16,12 +14,10 @@ public record TripRequest(
         String name,
 
         @NotNull(message = "여행 시작 시간을 입력해야 합니다. (예: 2023-10-25 12:00)")
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-        LocalDateTime startDateTime,
+        String startDateTime,
 
         @NotNull(message = "여행 종료 시간을 입력해야 합니다. (예: 2023-10-25 12:00)")
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-        LocalDateTime endDateTime,
+        String endDateTime,
 
         @NotNull(message = "국내외여부를 입력해야 합니다. (예: DOMESTIC)")
         TripType tripType
@@ -32,8 +28,8 @@ public record TripRequest(
                 .tripType(request.tripType)
                 .tripSchedule(
                         TimeScheduleInfo.builder()
-                                .startDateTime(LocalDateTime.from(request.startDateTime))
-                                .endDateTime(LocalDateTime.from(request.endDateTime))
+                                .startDateTime(DateTimeUtil.toLocalDateTime(request.startDateTime))
+                                .endDateTime(DateTimeUtil.toLocalDateTime(request.endDateTime))
                                 .build()
                 ).build();
     }
