2e06822197100e3723dd77e6cdd3a1d99e8f708d
ktpark <qkrrudxkr77@naver.com>
2023-10-26 18:11:00 +0900

    [fix] 패턴 애노테이션 정규식 수정
    Motivation
    - @Pattern 정규식 문제 수정
    
    Modification
    - @Pattern 정규식 변경

--- src/main/java/com/kdt_y_be_toy_project2/domain/trip/dto/TripRequest.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/dto/TripRequest.java
@@ -1,6 +1,5 @@
 package com.kdt_y_be_toy_project2.domain.trip.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kdt_y_be_toy_project2.domain.model.DateScheduleInfo;
 import com.kdt_y_be_toy_project2.domain.trip.domain.Trip;
 import com.kdt_y_be_toy_project2.domain.trip.domain.TripType;
@@ -9,7 +8,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
-import java.time.LocalDate;
 
 @Builder
 public record TripRequest(
--- src/main/java/com/kdt_y_be_toy_project2/global/util/DateTimeUtil.java
+++ src/main/java/com/kdt_y_be_toy_project2/global/util/DateTimeUtil.java
@@ -6,9 +6,9 @@ import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
 
-    public final static String LOCAL_DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm";
+    public final static String LOCAL_DATE_TIME_PATTERN = "yyyy-MM-dd";
 
-    public final static String LOCAL_DATE_REGEX_PATTERN = "(19|20)\\d{2}(\\/|-)(0[1-9]|1[1,2])(\\/|-)(0[1-9]|[12][0-9]|3[01])";
+    public final static String LOCAL_DATE_REGEX_PATTERN = "\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])";
 
     public static String toString(LocalDate dateTimeObject) {
         return dateTimeObject.format(DateTimeFormatter.ofPattern(LOCAL_DATE_TIME_PATTERN));
