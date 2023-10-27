c8dac1cdea22f6895f07111b98af65b472b2abb0
rightpair <tkddn204@gmail.com>
2023-10-27 11:06:33 +0900

    [fix] 여행 도메인의 DTO의 TripType 수정
    Motivation:
    - #58을 참고해주세요.
    
    Modification:
    - TripRequest의 tripType 필드의 자료형을 String으로 수정
    - TripResponse의 tripType 필드의 자료형을 String으로 수정
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/kdt_y_be_toy_project2/domain/trip/dto/TripRequest.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/dto/TripRequest.java
@@ -4,6 +4,7 @@ import com.kdt_y_be_toy_project2.domain.model.DateScheduleInfo;
 import com.kdt_y_be_toy_project2.domain.trip.domain.Trip;
 import com.kdt_y_be_toy_project2.domain.trip.domain.TripType;
 import com.kdt_y_be_toy_project2.global.util.DateTimeUtil;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -11,29 +12,29 @@ import lombok.Builder;
 
 @Builder
 public record TripRequest(
-        @NotBlank(message = "여행 이름을 입력해야 합니다.")
-        String name,
+	@NotBlank(message = "여행 이름을 입력해야 합니다.")
+	String name,
 
-        @NotNull(message = "여행 시작 시간을 입력해야 합니다.")
-        @Pattern(regexp = DateTimeUtil.LOCAL_DATE_REGEX_PATTERN, message = "잘못된 시간 형식입니다. (올바른 예시: 2023-10-25)")
-        String startDate,
+	@NotNull(message = "여행 시작 시간을 입력해야 합니다.")
+	@Pattern(regexp = DateTimeUtil.LOCAL_DATE_REGEX_PATTERN, message = "잘못된 시간 형식입니다. (올바른 예시: 2023-10-25)")
+	String startDate,
 
-        @NotNull(message = "여행 종료 시간을 입력해야 합니다. (예: 2023-10-25 12:00)")
-        @Pattern(regexp = DateTimeUtil.LOCAL_DATE_REGEX_PATTERN, message = "잘못된 시간 형식입니다. (올바른 예시: 2023-10-25)")
-        String endDate,
+	@NotNull(message = "여행 종료 시간을 입력해야 합니다.")
+	@Pattern(regexp = DateTimeUtil.LOCAL_DATE_REGEX_PATTERN, message = "잘못된 시간 형식입니다. (올바른 예시: 2023-10-25)")
+	String endDate,
 
-        @NotNull(message = "국내외여부를 입력해야 합니다. (예: DOMESTIC)")
-        TripType tripType
+	@NotNull(message = "국내외여부를 입력해야 합니다. (예: 국내)")
+	String tripType
 ) {
-    public static Trip toEntity(final TripRequest request) {
-        return Trip.builder()
-                .name(request.name)
-                .tripType(request.tripType)
-                .tripSchedule(
-                        DateScheduleInfo.builder()
-                                .startDate(DateTimeUtil.toLocalDateTime(request.startDate))
-                                .endDate(DateTimeUtil.toLocalDateTime(request.endDate))
-                                .build()
-                ).build();
-    }
+	public static Trip toEntity(final TripRequest request) {
+		return Trip.builder()
+			.name(request.name)
+			.tripType(TripType.getByValue(request.tripType))
+			.tripSchedule(
+				DateScheduleInfo.builder()
+					.startDate(DateTimeUtil.toLocalDateTime(request.startDate))
+					.endDate(DateTimeUtil.toLocalDateTime(request.endDate))
+					.build()
+			).build();
+	}
 }
