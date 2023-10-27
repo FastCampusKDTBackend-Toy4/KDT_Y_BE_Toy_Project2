9fad58617cf58e3c2be334cd5c2c01d4e3721d3b
ktpark <qkrrudxkr77@naver.com>
2023-10-27 17:28:49 +0900

    [test] 여행 서비스 유닛 테스트 코드 작성
    Motivation:
    - 여행 서비스의 유닛 테스트
    
    Modification:
    - 여행 서비스 클래스에 있는 메소드에 대한 테스트

--- src/main/java/com/kdt_y_be_toy_project2/domain/trip/service/TripService.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/service/TripService.java
@@ -1,58 +1,58 @@
 package com.kdt_y_be_toy_project2.domain.trip.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.kdt_y_be_toy_project2.domain.trip.domain.Trip;
 import com.kdt_y_be_toy_project2.domain.trip.dto.TripRequest;
 import com.kdt_y_be_toy_project2.domain.trip.dto.TripResponse;
 import com.kdt_y_be_toy_project2.domain.trip.exception.TripNotFoundException;
 import com.kdt_y_be_toy_project2.domain.trip.repository.TripRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class TripService {
 
-    private final TripRepository tripRepository;
+	private final TripRepository tripRepository;
 
-    @Transactional(readOnly = true)
-    public List<TripResponse> getAllTrips() {
-        List<Trip> trips = tripRepository.findAll();
+	@Transactional(readOnly = true)
+	public List<TripResponse> getAllTrips() {
+		List<Trip> trips = tripRepository.findAll();
 
-        if (trips.isEmpty()) {
-            throw new TripNotFoundException();
-        }
-        return trips
-                .stream().map(TripResponse::from)
-                .toList();
-    }
+		if (trips.isEmpty()) {
+			throw new TripNotFoundException();
+		}
+		return trips
+			.stream().map(TripResponse::from)
+			.toList();
+	}
 
-    @Transactional(readOnly = true)
-    public TripResponse getTripById(final Long tripId) {
-        return tripRepository.findById(tripId)
-                .map(TripResponse::from)
-                .orElseThrow(TripNotFoundException::new);
-    }
+	@Transactional(readOnly = true)
+	public TripResponse getTripById(final Long tripId) {
+		return tripRepository.findById(tripId)
+			.map(TripResponse::from)
+			.orElseThrow(TripNotFoundException::new);
+	}
 
-    public TripResponse createTrip(final TripRequest request) {
-        return Optional.of(tripRepository.save(TripRequest.toEntity(request)))
-                .map(TripResponse::from)
-                .orElseThrow();
-    }
+	public TripResponse createTrip(final TripRequest request) {
+		return Optional.of(tripRepository.save(TripRequest.toEntity(request)))
+			.map(TripResponse::from)
+			.orElseThrow();
+	}
 
-    public TripResponse editTrip(final Long tripId, final TripRequest request) {
-        Trip updatedTrip = tripRepository.findById(tripId)
-                .map(trip -> trip.update(TripRequest.toEntity(request)))
-                .orElseThrow(TripNotFoundException::new);
+	public TripResponse editTrip(final Long tripId, final TripRequest request) {
+		Trip updatedTrip = tripRepository.findById(tripId)
+			.map(trip -> trip.update(TripRequest.toEntity(request)))
+			.orElseThrow(TripNotFoundException::new);
 
-        return Optional.of(tripRepository.save(updatedTrip))
-                .map(TripResponse::from)
-                .orElseThrow();
-    }
+		return TripResponse.from(updatedTrip);
+	}
 }
 
 
