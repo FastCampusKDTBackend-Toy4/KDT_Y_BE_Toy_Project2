f9222d6aac329d08bb71124402a3eebc772d0150
rightpair <tkddn204@gmail.com>
2023-10-27 21:27:17 +0900

    [test] 여행 도메인 통합 테스트(실패케이스) 작성
    Motivation:
    - 여행 도메인의 컨트롤러 통합 테스트의 실패 케이스 코드를 작성하려 합니다.
    
    Modification:
    - `TripController`의 메소드의 실패 케이스 통합 테스트 코드 작성
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/test/java/com/kdt_y_be_toy_project2/domain/trip/controller/TripControllerTest.java
+++ src/test/java/com/kdt_y_be_toy_project2/domain/trip/controller/TripControllerTest.java
@@ -1,11 +1,9 @@
 package com.kdt_y_be_toy_project2.domain.trip.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.util.List;
-
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -18,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kdt_y_be_toy_project2.domain.trip.domain.Trip;
@@ -43,7 +42,6 @@ class TripControllerTest {
 	private void assertTripResponse(Trip expectedTrip, ResultActions resultActions) throws Exception {
 		// then
 		resultActions
-			.andDo(print())
 			.andExpect(jsonPath("$.tripName").value(expectedTrip.getName()))
 			.andExpect(jsonPath("$.tripType").value(expectedTrip.getTripType().getValue()))
 			.andExpect(jsonPath("$.startDate")
@@ -56,22 +54,29 @@ class TripControllerTest {
 				));
 	}
 
+	// // 아래 테스트를 수행하려면, 여행 리스트가 비어 있어야 되기 때문에 독립적으로 수행되어야 합니다.
+	// @DisplayName("모든 여행 정보를 가져올 때 여행 정보 리스트가 비어 있을 경우 에러를 반환한다.")
+	// @Disabled
+	// @Test
+	// void shouldFailToGetAllTripsWithEmptyList() throws Exception {
+	// 	// when
+	// 	ResultActions getAllTripsAction = mockMvc.perform(get("/v1/trips"));
+	//
+	// 	// then
+	// 	getAllTripsAction
+	// 		.andExpect(status().isNotFound())
+	// 		.andExpect(jsonPath("message").value("여행 정보를 찾을 수 없습니다."));
+	// }
+
 	@DisplayName("여행 정보를 조회할 때")
-	@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 	@Nested
 	class GetTripsTest {
-
-		private List<Trip> savedTripList;
-
-		@BeforeAll
-		void beforeAll() {
-			// given
-			savedTripList = tripRepository.saveAll(TripTestFactory.createTestTripList(5));
-		}
-
 		@DisplayName("모든 여행 정보를 가져올 수 있다.")
 		@Test
 		void shouldSuccessToGetAllTrips() throws Exception {
+			// given
+			tripRepository.saveAll(TripTestFactory.createTestTripList(5));
+
 			// when
 			ResultActions getAllTripsAction = mockMvc.perform(get("/v1/trips"));
 
@@ -85,7 +90,7 @@ class TripControllerTest {
 		@Test
 		void shouldSuccessToGetTripById() throws Exception {
 			// given
-			Trip expectedTrip = savedTripList.get(0);
+			Trip expectedTrip = tripRepository.save(TripTestFactory.createTestTrip());
 			long tripId = expectedTrip.getId();
 
 			// when
@@ -94,6 +99,21 @@ class TripControllerTest {
 			// then
 			getTripAction.andExpect(status().isOk());
 			assertTripResponse(expectedTrip, getTripAction);
+		}
+
+		@DisplayName("존재하지 않는 ID의 여행 정보를 가져올 수 없다.")
+		@Test
+		void shouldFailToGetTripByIdWithInvalidId() throws Exception {
+			// give
+			long tripId = 12345L;
+
+			// when
+			ResultActions getTripAction = mockMvc.perform(get("/v1/trips/" + tripId));
+
+			// then
+			getTripAction
+				.andExpect(status().isNotFound())
+				.andExpect(jsonPath("message").value("여행 정보를 찾을 수 없습니다."));
 		}
 	}
 
@@ -123,6 +143,81 @@ class TripControllerTest {
 			// then
 			createTripAction.andExpect(status().isCreated());
 			assertTripResponse(expectedTrip, createTripAction);
+		}
+
+		@DisplayName("여행 이름을 입력하지 않으면 등록할 수 없다.")
+		@Test
+		void shouldFailToCreateTripWithEmptyName() throws Exception {
+			// given
+			Trip expectedTrip = TripTestFactory.createTestTrip();
+			String startDateTimeStr = DateTimeUtil.toString(expectedTrip.getTripSchedule().getStartDate());
+			String endDateTimeStr = DateTimeUtil.toString(expectedTrip.getTripSchedule().getEndDate());
+			TripRequest request = TripRequest.builder()
+				.tripType(expectedTrip.getTripType().getValue())
+				.startDate(startDateTimeStr)
+				.endDate(endDateTimeStr)
+				.build();
+
+			// when
+			ResultActions createTripAction = mockMvc.perform(post("/v1/trips")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)));
+
+			// then
+			createTripAction
+				.andExpect(status().isBadRequest())
+				.andExpect(res -> Assertions.assertThat(res.getResolvedException())
+					.isInstanceOf(MethodArgumentNotValidException.class));
+		}
+
+		@DisplayName("여행이 끝나는 시간이 시작 시간보다 작으면 여행을 등록할 수 없다.")
+		@Test
+		void shouldFailToCreateTripIfEndTimeIsBeforeStartTime() throws Exception {
+			// given
+			Trip expectedTrip = TripTestFactory.createTestTrip();
+			String startDateTimeStr = DateTimeUtil.toString(expectedTrip.getTripSchedule().getEndDate());
+			String endDateTimeStr = DateTimeUtil.toString(expectedTrip.getTripSchedule().getStartDate());
+			TripRequest request = TripRequest.builder()
+				.name(expectedTrip.getName())
+				.tripType(expectedTrip.getTripType().getValue())
+				.startDate(startDateTimeStr)
+				.endDate(endDateTimeStr)
+				.build();
+
+			// when
+			ResultActions createTripAction = mockMvc.perform(post("/v1/trips")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)));
+
+			// then
+			createTripAction
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("message").value("날짜 범위가 잘못 선택되었습니다."));
+		}
+
+		@DisplayName("잘못된 여행 타입을 입력하면 등록할 수 없다.")
+		@Test
+		void shouldFailToCreateTripWithInvalidTripType() throws Exception {
+			// given
+			Trip expectedTrip = TripTestFactory.createTestTrip();
+			String startDateTimeStr = DateTimeUtil.toString(expectedTrip.getTripSchedule().getStartDate());
+			String endDateTimeStr = DateTimeUtil.toString(expectedTrip.getTripSchedule().getEndDate());
+			TripRequest request = TripRequest.builder()
+				.tripType("잘못된여행타입")
+				.startDate(startDateTimeStr)
+				.endDate(endDateTimeStr)
+				.build();
+
+			// when
+			ResultActions createTripAction = mockMvc.perform(post("/v1/trips")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)));
+
+			// then
+			createTripAction
+				.andExpect(status().isBadRequest())
+				.andExpect(res -> Assertions.assertThat(res.getResolvedException())
+					.isInstanceOf(MethodArgumentNotValidException.class));
 		}
 	}
 
@@ -163,6 +258,32 @@ class TripControllerTest {
 			editTripAction.andExpect(status().isOk())
 				.andExpect(jsonPath("$.tripId").value(savedTripId));
 			assertTripResponse(expectedTrip, editTripAction);
+		}
+
+		@DisplayName("여행이 끝나는 시간이 시작 시간보다 작으면 여행을 수정할 수 없다.")
+		@Test
+		void shouldFailToCreateTripIfEndTimeIsBeforeStartTime() throws Exception {
+			// given
+			long savedTripId = savedTrip.getId();
+			Trip expectedTrip = TripTestFactory.createTestTrip();
+			String startDateTimeStr = DateTimeUtil.toString(expectedTrip.getTripSchedule().getEndDate());
+			String endDateTimeStr = DateTimeUtil.toString(expectedTrip.getTripSchedule().getStartDate());
+			TripRequest request = TripRequest.builder()
+				.name(expectedTrip.getName())
+				.tripType(expectedTrip.getTripType().getValue())
+				.startDate(startDateTimeStr)
+				.endDate(endDateTimeStr)
+				.build();
+
+			// when
+			ResultActions editTripAction = mockMvc.perform(put("/v1/trips/" + savedTripId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)));
+
+			// then
+			editTripAction
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("message").value("날짜 범위가 잘못 선택되었습니다."));
 		}
 	}
 }
