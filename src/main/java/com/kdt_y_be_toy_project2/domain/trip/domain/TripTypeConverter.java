65703ae7a383d71548814f22701236030bd0ff67
rightpair <tkddn204@gmail.com>
2023-11-10 13:02:18 +0900

    [fix] 국내외여부 enum 변환 클래스에 `@Converter` 주석 추가
    Motivation:
    - #80 을 참고해주세요.
    
    Modification:
    - `TripTypeConverter.java`에 `@Converter` 주석 추가
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/kdt_y_be_toy_project2/domain/trip/domain/TripTypeConverter.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/domain/TripTypeConverter.java
@@ -1,18 +1,20 @@
 package com.kdt_y_be_toy_project2.domain.trip.domain;
 
 import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
 
+@Converter
 public class TripTypeConverter implements AttributeConverter<TripType, String> {
-    @Override
-    public String convertToDatabaseColumn(TripType attribute) {
-        if (attribute == null) {
-            throw new NullPointerException("TripType이 비어 있습니다.");
-        }
-        return attribute.getValue();
-    }
+	@Override
+	public String convertToDatabaseColumn(TripType attribute) {
+		if (attribute == null) {
+			throw new NullPointerException("TripType이 비어 있습니다.");
+		}
+		return attribute.getValue();
+	}
 
-    @Override
-    public TripType convertToEntityAttribute(String dbData) {
-        return TripType.getByValue(dbData);
-    }
+	@Override
+	public TripType convertToEntityAttribute(String dbData) {
+		return TripType.getByValue(dbData);
+	}
 }
