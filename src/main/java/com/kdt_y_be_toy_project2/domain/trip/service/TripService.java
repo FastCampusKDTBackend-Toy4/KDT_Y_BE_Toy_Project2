a203354576ae7a37a6968801efbd3060b3a2e1f7
rightpair <tkddn204@gmail.com>
2023-11-13 15:15:09 +0900

    [feat] 여행 검색 Repository 추가
    Motivation:
    - #91 에 따라 여행 검색 기능을 추가하려 합니다.
    - 여행 컨트롤러에서 사용하게 될 서비스 코드를 작성하려 합니다.
    - 서비스 코드에서 사용하게 될 요청 객체의 코드를 수정하여 작성하려 합니다.
    - 서비스에서 여행 검색 요청에 대한 검증을 하는 코드를 포함해서 작성하려 합니다.
    
    Modification:
    - `TripService.java`에서 searchTrips 메소드 추가
    - `TripSearchRequest.java` 수정
    - searchTrips 메소드에서 요청을 검증할 validation 관련 메소드 추가
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/kdt_y_be_toy_project2/domain/trip/service/TripService.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/service/TripService.java
@@ -1,14 +1,18 @@
 package com.kdt_y_be_toy_project2.domain.trip.service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.kdt_y_be_toy_project2.domain.model.exception.InvalidDateRangeException;
 import com.kdt_y_be_toy_project2.domain.trip.domain.Trip;
 import com.kdt_y_be_toy_project2.domain.trip.dto.TripRequest;
 import com.kdt_y_be_toy_project2.domain.trip.dto.TripResponse;
+import com.kdt_y_be_toy_project2.domain.trip.dto.TripSearchRequest;
 import com.kdt_y_be_toy_project2.domain.trip.exception.TripNotFoundException;
 import com.kdt_y_be_toy_project2.domain.trip.repository.TripRepository;
 
@@ -53,7 +57,40 @@ public class TripService {
 
 		return TripResponse.from(updatedTrip);
 	}
+
+	public List<TripResponse> searchTrips(TripSearchRequest request) {
+		validateSearchTripRequest(request);
+		return tripRepository.searchTrips(
+				request.name(),
+				request.getTripType(),
+				request.getStartDate(),
+				request.getEndDate()
+			)
+			.stream().map(TripResponse::from)
+			.toList();
+	}
+
+	private void validateSearchTripRequest(TripSearchRequest request) {
+		if (isAllValidDate(request.getStartDate(), request.getEndDate())) {
+			validateDateRange(
+				Objects.requireNonNull(request.getStartDate()),
+				Objects.requireNonNull(request.getEndDate())
+			);
+		}
+	}
+
+	private boolean isAllValidDate(LocalDate start, LocalDate end) {
+		return start != null && end != null;
+	}
+
+	private void validateDateRange(LocalDate start, LocalDate end) {
+		if (start.isAfter(end) && isDayTrip(start, end)) {
+			throw new InvalidDateRangeException("startDate 는 항상 endDate 보다 과거여야 합니다. "
+				+ "{\"startDate = \" %s / endDate = %s}".formatted(start, end));
+		}
+	}
+
+	private boolean isDayTrip(LocalDate start, LocalDate end) {
+		return !start.equals(end);
+	}
 }
-
-
-
