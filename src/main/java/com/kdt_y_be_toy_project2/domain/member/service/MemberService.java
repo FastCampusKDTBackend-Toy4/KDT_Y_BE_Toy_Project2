43a251e1286e5ecd3e32c5da4e254717afe32dff
Nine-JH <rnwkgus2@gmail.com>
2023-11-13 23:36:16 +0900

    [refactor] FormLogin 재등록
    motivation:
    - Provider을 추가하면서 ProviderManager에 추가가 되지 않는 현상을 발견하였고 이를 해결하지 못해 임시적으로 RestAPI를 제공함으로써 우회했습니다. 하지만위 문제를 해결했기때문에 다시 SecurityFilterChain을 사용합니다.
    
    modification:
    - Rest API Login 방식을 Security로 재등록

--- src/main/java/com/kdt_y_be_toy_project2/domain/member/service/MemberService.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/member/service/MemberService.java
@@ -1,13 +1,8 @@
 package com.kdt_y_be_toy_project2.domain.member.service;
 
 import com.kdt_y_be_toy_project2.domain.member.domain.Member;
-import com.kdt_y_be_toy_project2.domain.member.dto.request.LoginRequest;
-import com.kdt_y_be_toy_project2.domain.member.dto.request.LoginResponse;
 import com.kdt_y_be_toy_project2.domain.member.dto.request.SignUpRequest;
 import com.kdt_y_be_toy_project2.domain.member.repository.MemberRepository;
-import com.kdt_y_be_toy_project2.global.jwt.JwtPayload;
-import com.kdt_y_be_toy_project2.global.jwt.JwtProvider;
-import java.util.Date;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +11,10 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
-    private final JwtProvider jwtProvider;
 
-    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder,
-        JwtProvider jwtProvider) {
+    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
         this.passwordEncoder = passwordEncoder;
-        this.jwtProvider = jwtProvider;
     }
 
     public void signUp(SignUpRequest request) {
@@ -31,21 +23,5 @@ public class MemberService {
             .password(passwordEncoder.encode(request.password()))
             .name(request.name())
             .build());
-    }
-
-    public LoginResponse login(LoginRequest loginRequest) {
-        Member member = memberRepository.findById(loginRequest.username())
-            .orElseThrow(() -> new IllegalArgumentException("member not found"));
-
-        if (!passwordEncoder.matches(loginRequest.password(), member.getPassword())) {
-            throw new IllegalArgumentException("Not match password");
-        }
-
-        String accessToken = jwtProvider.createAccessToken(
-            new JwtPayload(loginRequest.username(), new Date()));
-        String refreshToken = jwtProvider.createRefreshToken(
-            new JwtPayload(loginRequest.username(), new Date()));
-
-        return new LoginResponse(loginRequest.username(), accessToken, refreshToken);
     }
 }
---
+++ src/main/java/com/kdt_y_be_toy_project2/global/security/formlogin/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.kdt_y_be_toy_project2.global.security.formlogin;
+
+import com.kdt_y_be_toy_project2.domain.member.domain.Member;
+import com.kdt_y_be_toy_project2.domain.member.repository.MemberRepository;
+import java.util.Set;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    public CustomUserDetailsService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findById(username)
+            .orElseThrow(() -> new IllegalArgumentException("member not found"));
+
+        return new AccountContext(member.getEmail(), member.getPassword(),
+            Set.of(new SimpleGrantedAuthority("ROLE_USER")));
+    }
+}
