66533bc7f934f7be508ca6a4e7efe47fb40405ac
rightpair <tkddn204@gmail.com>
2023-11-13 16:26:25 +0900

    [feat] 여행 레포지토리의 `getAllTripByMemberEmail` 메소드에 쿼리 어노테이션 추가
    Motivation:
    - #85 에 따라 좋아요 관련 기능을 개발하려 합니다.
    - **멤버가 있음을 가정**한 뒤 개발 중입니다.
    - N+1 문제를 해결하기 위해, 커스텀 JPQL로 쿼리를 보내려 합니다.
    
    Modification:
    - `getAllTripByMemberEmail` 메소드에 쿼리 어노테이션 추가
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/kdt_y_be_toy_project2/domain/trip/repository/LikesRepository.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/trip/repository/LikesRepository.java
@@ -3,14 +3,18 @@ package com.kdt_y_be_toy_project2.domain.trip.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.kdt_y_be_toy_project2.domain.trip.domain.Likes;
+import com.kdt_y_be_toy_project2.domain.trip.domain.Trip;
 import com.kdt_y_be_toy_project2.domain.trip.domain.id.LikesID;
 
 @Repository
 public interface LikesRepository extends JpaRepository<Likes, LikesID> {
 	Long countByTripId(Long tripId);
 
-	List<Likes> getAllTripByMemberEmail(String memberEmail);
+	@Query("select l.trip from Likes l where l.member.email = :email")
+	List<Trip> getAllTripByMemberEmail(@Param("email") String memberEmail);
 }
