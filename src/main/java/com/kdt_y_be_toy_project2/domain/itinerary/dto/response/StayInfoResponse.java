260c5f8af2f55336252141f70b7709eef0c55f5b
minkyun <minkyun@kakao.com>
2023-11-14 12:23:50 +0900

    [refactor] PlaceInfo 개선으로 인한 Response 수정
    Motivation:
    
     - #93  PlaceInfo 수정으로인해 변경된 Itinerary Response 값을 수정하려 합니다.
    
    Modification:
    
     - AccommodationInfoResponse.java PlaceInfo 수정
     - MoveInfoResponse.java PlaceInfo 수정
     - StayInfoResponse.java PlaceInfo 수정

--- src/main/java/com/kdt_y_be_toy_project2/domain/itinerary/dto/response/StayInfoResponse.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/itinerary/dto/response/StayInfoResponse.java
@@ -1,6 +1,7 @@
 package com.kdt_y_be_toy_project2.domain.itinerary.dto.response;
 
 import com.kdt_y_be_toy_project2.domain.itinerary.domain.StayInfo;
+import com.kdt_y_be_toy_project2.domain.model.PlaceInfo;
 import com.kdt_y_be_toy_project2.global.util.LocalDateTimeUtil;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
@@ -13,13 +14,13 @@ public record StayInfoResponse(
         @Schema(description = "여가 종료 시간")
         String endDateTime,
         @Schema(description = "여가 장소")
-        String stayPlaceName
+        PlaceInfo stayPlaceName
 ) {
     public static StayInfoResponse from(final StayInfo stayInfo) {
         return StayInfoResponse.builder()
                 .startDateTime(LocalDateTimeUtil.toString(stayInfo.getStaySchedule().getStartDateTime()))
                 .endDateTime(LocalDateTimeUtil.toString(stayInfo.getStaySchedule().getEndDateTime()))
-                .stayPlaceName(stayInfo.getStayPlaceInfo().getName())
+                .stayPlaceName(stayInfo.getStayPlaceInfo())
                 .build();
     }
 }
