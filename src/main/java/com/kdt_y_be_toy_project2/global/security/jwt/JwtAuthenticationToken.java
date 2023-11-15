15fad9bae260f23b4d72d4cf66c54668b512f366
Nine-JH <rnwkgus2@gmail.com>
2023-11-15 15:10:35 +0900

    [refactor] JwtFilter 의존성 변경
    motivation:
    - `JwtProvider` 대신 `JwtService`를 사용하기 때문에 의존성 변경을 합니다.
    - filter에서 매번 refreshToken을 받지 않기 때문에 인증로직에서도 refreshToken을 제외합니다.
    
    modification:
    - 인증 로직에서 refreshToken 제외
    - jwtProvider 의존성 변경

--- src/main/java/com/kdt_y_be_toy_project2/global/security/jwt/JwtAuthenticationToken.java
+++ src/main/java/com/kdt_y_be_toy_project2/global/security/jwt/JwtAuthenticationToken.java
@@ -1,36 +1,34 @@
 package com.kdt_y_be_toy_project2.global.security.jwt;
 
+import java.util.Objects;
 import java.util.Set;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public class JwtAuthenticationToken extends AbstractAuthenticationToken {
 
-    private String principal;
-    private JwtPair credentials;
+    private final String principal;
+    private String credentials;
     private final boolean isAuthenticated;
 
-    private JwtAuthenticationToken(String accessToken, String refreshToken, boolean isAuthenticated) {
+    private JwtAuthenticationToken(String principal, String credentials, boolean isAuthenticated) {
         super(Set.of(new SimpleGrantedAuthority("ROLE_USER")));
-        this.credentials = new JwtPair(accessToken, refreshToken);
+        this.principal = principal;
+        this.credentials = credentials;
         this.isAuthenticated = isAuthenticated;
     }
 
-    private JwtAuthenticationToken(String email, boolean isAuthenticated) {
+    private JwtAuthenticationToken(String principal, boolean isAuthenticated) {
         super(Set.of(new SimpleGrantedAuthority("ROLE_USER")));
-        this.principal = email;
+        this.principal = principal;
         this.isAuthenticated = isAuthenticated;
     }
 
-    public static JwtAuthenticationToken emptyToken() {
-        return new JwtAuthenticationToken(null, null, false);
+    public static JwtAuthenticationToken unAuthorize(String accessToken) {
+        return new JwtAuthenticationToken(null, accessToken, false);
     }
 
-    public static JwtAuthenticationToken unAuthorizeToken(String accessToken, String refreshToken) {
-        return new JwtAuthenticationToken(accessToken, refreshToken, false);
-    }
-
-    public static JwtAuthenticationToken authorizeToken(String email) {
+    public static JwtAuthenticationToken authorize(String email) {
         return new JwtAuthenticationToken(email, true);
     }
 
@@ -40,12 +38,33 @@ public class JwtAuthenticationToken extends AbstractAuthenticationToken {
     }
 
     @Override
-    public JwtPair getCredentials() {
+    public String getCredentials() {
         return credentials;
     }
 
     @Override
     public boolean isAuthenticated() {
         return isAuthenticated;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        if (!super.equals(object)) {
+            return false;
+        }
+        JwtAuthenticationToken that = (JwtAuthenticationToken) object;
+        return isAuthenticated == that.isAuthenticated && Objects.equals(principal,
+            that.principal) && Objects.equals(credentials, that.credentials);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), principal, credentials, isAuthenticated);
     }
 }
