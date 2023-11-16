52b8bd75c77cb90fa09484a8f52e2297c07ff1e6
rightpair <tkddn204@gmail.com>
2023-11-16 15:44:40 +0900

    [fix] 여행 컨트롤러 유닛, 통합 테스트 오류 수정
    Motivation:
    - #140 관련해서 발생했던 오류 2에 대해 수정한 코드를 작성하려 합니다.
    
    Modification:
    - `@BeforeAll`을 사용하는 코드를 `@BeforeEach`로 바꾸어주었습니다.
    - `@MockBean(JpaMetamodelMappingContext.class)`를 추가하여 JPA meta의 값을 mock 값으로 넣어줄 수 있도록 수정했습니다.
    - `MemberTestFactory`에서 랜덤 범위를 늘려 중복되는 값이 나올 확률을 줄일 수 있도록 했습니다.
    -
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/test/java/com/kdt_y_be_toy_project2/global/factory/MemberTestFactory.java
+++ src/test/java/com/kdt_y_be_toy_project2/global/factory/MemberTestFactory.java
@@ -1,7 +1,5 @@
 package com.kdt_y_be_toy_project2.global.factory;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,25 +13,9 @@ public class MemberTestFactory {
 
 	public static Member createTestMemberWithRandomPassword() {
 		return Member.builder()
-			.name("멤버테스트" + ThreadLocalRandom.current().nextInt(1000))
-			.email("test" + ThreadLocalRandom.current().nextInt(1000) + "@test.com")
+			.name("멤버테스트" + ThreadLocalRandom.current().nextInt(1000000))
+			.email("test" + ThreadLocalRandom.current().nextInt(1000000) + "@test.com")
 			.password(passwordEncoder.encode(String.valueOf(ThreadLocalRandom.current().nextInt())))
 			.build();
-	}
-
-	public static Member createTestMemberWithPassword(String password) {
-		return Member.builder()
-			.name("멤버테스트" + ThreadLocalRandom.current().nextInt(1000))
-			.email("test" + ThreadLocalRandom.current().nextInt(1000) + "@test.com")
-			.password(passwordEncoder.encode(password))
-			.build();
-	}
-
-	public static List<Member> createTestMemberList(int size) {
-		List<Member> memberList = new ArrayList<>();
-		while (size-- > 0) {
-			memberList.add(createTestMemberWithRandomPassword());
-		}
-		return memberList;
 	}
 }
