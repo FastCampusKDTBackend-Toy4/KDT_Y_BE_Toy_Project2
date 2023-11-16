4a77c4e0e5dc6d513f435539f9e981d62aaad618
rightpair <tkddn204@gmail.com>
2023-11-16 11:07:25 +0900

    [fix] 댓글 관련 엔티티 수정
    Motivation:
    - #135 를 참고하세요.
    
    Modification:
    - Comment, Trip, Member 엔티티 수정
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/kdt_y_be_toy_project2/domain/member/domain/Member.java
+++ src/main/java/com/kdt_y_be_toy_project2/domain/member/domain/Member.java
@@ -1,40 +1,28 @@
 package com.kdt_y_be_toy_project2.domain.member.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.kdt_y_be_toy_project2.domain.trip.domain.Comment;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@EqualsAndHashCode(of = "email")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Member {
 
-    @Id
-    private String email;
+	@Id
+	private String email;
 
-    private String password;
+	private String password;
 
-    private String name;
+	private String name;
 
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-    List<Comment> comments = new ArrayList<>();
-
-    @Builder
-    private Member(String email, String password, String name) {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-    }
+	@Builder
+	private Member(String email, String password, String name) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+	}
 }
