61e4ea7e3484f1b53b6cf31d4aadabbe455801a3
jaecheol sim <wocjf0513@ajou.ac.kr>
2023-11-16 10:18:44 +0900

    [fix] 여정 controller 유닛 테스트 수정
    Motivation:
    
     - controller관련 빈만 설정되게 webMVCTEST 어노테이션을 사용했는데,
       거기에 jpa model까지 공짜로 불러오려다보니 잘못됐습니다.
    
    Modification:
    
    - MockBean(JpaMetaModelMapping~) 어노테이션 추가 및 test내에 member repository삭제

--- src/test/java/com/kdt_y_be_toy_project2/domain/itinerary/controller/ItineraryControllerTest.java
+++ src/test/java/com/kdt_y_be_toy_project2/domain/itinerary/controller/ItineraryControllerTest.java
@@ -1,20 +1,5 @@
 package com.kdt_y_be_toy_project2.domain.itinerary.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
-import java.time.LocalDateTime;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-
 import com.kdt_y_be_toy_project2.domain.itinerary.domain.Itinerary;
 import com.kdt_y_be_toy_project2.domain.itinerary.dto.ItineraryRequest;
 import com.kdt_y_be_toy_project2.domain.itinerary.dto.ItineraryResponse;
@@ -26,52 +11,65 @@ import com.kdt_y_be_toy_project2.global.factory.ItineraryTestFactory;
 import com.kdt_y_be_toy_project2.global.factory.MemberTestFactory;
 import com.kdt_y_be_toy_project2.global.factory.TripTestFactory;
 import com.kdt_y_be_toy_project2.global.resolver.LoginInfo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @WebMvcTest(ItineraryController.class)
 @ExtendWith(MockitoExtension.class)
+@MockBean(JpaMetamodelMappingContext.class)
 class ItineraryControllerTest {
 
-	@MockBean
-	private ItineraryService itineraryService;
+    @MockBean
+    private ItineraryService itineraryService;
 
-	@Autowired
-	private ItineraryController itineraryController;
+    @Autowired
+    private ItineraryController itineraryController;
 
-	@Autowired
-	private MemberRepository memberRepository;
+    private ItineraryRequest itineraryRequest;
 
-	private ItineraryRequest itineraryRequest;
+    private Itinerary itinerary;
 
-	private Itinerary itinerary;
+    private LoginInfo loginInfo;
 
-	private LoginInfo loginInfo;
+    @BeforeEach
+    void initItinerary() {
+        Member member = MemberTestFactory.createTestMemberWithRandomPassword();
+        itineraryRequest = ItineraryTestFactory
+                .buildItineraryRequest(DateTimeScheduleInfo.builder()
+                        .startDateTime(LocalDateTime.now())
+                        .endDateTime(LocalDateTime.now())
+                        .build());
+        itinerary = ItineraryRequest.toEntity(itineraryRequest, TripTestFactory.createTestTrip(member));
+        loginInfo = new LoginInfo(itinerary.getTrip().getMember().getEmail());
+    }
 
-	@BeforeEach
-	void initItinerary() {
-		Member member = memberRepository.save(MemberTestFactory.createTestMemberWithRandomPassword());
-		itineraryRequest = ItineraryTestFactory
-			.buildItineraryRequest(DateTimeScheduleInfo.builder()
-				.startDateTime(LocalDateTime.now())
-				.endDateTime(LocalDateTime.now())
-				.build());
-		itinerary = ItineraryRequest.toEntity(itineraryRequest, TripTestFactory.createTestTrip(member));
-		loginInfo = new LoginInfo(itinerary.getTrip().getMember().getEmail());
-	}
+    @Test
+    void editItinerary() {
+        //given
+        ItineraryResponse itineraryResponse = ItineraryResponse.from(itinerary);
+        when(itineraryService
+                .editItinerary(loginInfo, 1L, 1L, itineraryRequest))
+                .thenReturn(itineraryResponse);
 
-	@Test
-	void editItinerary() {
-		//given
-		ItineraryResponse itineraryResponse = ItineraryResponse.from(itinerary);
-		when(itineraryService
-			.editItinerary(loginInfo, 1L, 1L, itineraryRequest))
-			.thenReturn(itineraryResponse);
+        //when
+        ResponseEntity<ItineraryResponse> responseResponseEntity = itineraryController.editItinerary(loginInfo,
+                1L, 1L, itineraryRequest);
 
-		//when
-		ResponseEntity<ItineraryResponse> responseResponseEntity = itineraryController.editItinerary(loginInfo,
-			1L, 1L, itineraryRequest);
-
-		//then
-		assertEquals(responseResponseEntity.getStatusCode(), HttpStatusCode.valueOf(200));
-		assertEquals(responseResponseEntity.getBody(), itineraryResponse);
-	}
+        //then
+        assertEquals(responseResponseEntity.getStatusCode(), HttpStatusCode.valueOf(200));
+        assertEquals(responseResponseEntity.getBody(), itineraryResponse);
+    }
 }
